3e6d79abd6abc27dee1c16ff342459932ba11c95
Yugo Yagita <2000ygkn0713@gmail.com>
2017-02-05 18:52:52 +0900

    Fix script

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 const $ = (id) => document.getElementById(id);
 
 const $form = $("post");
-const $fromText = $("fromText");
 const $inputText = $("inputText");
-const $fromImage = $("fromImage");
-const $inputImage = $("inputImage");
 
 const $renderer = $("renderer");
 
 const $deliverable = $("deliverable");
-const $copy = $("copy");
-const $download = $("download");
 
 const context = $renderer.getContext("2d");
 
@@ -19,25 +14,6 @@ const worker = new Worker("worker.js");
 context.textAlign = "center";
 context.font = "80px cinecaption"
 context.fillText("", 0, 0)
-
-function encode64(input) {
-	var output = "", i = 0, l = input.length,
-	key = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=", 
-	chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-	while (i < l) {
-		chr1 = input.charCodeAt(i++);
-		chr2 = input.charCodeAt(i++);
-		chr3 = input.charCodeAt(i++);
-		enc1 = chr1 >> 2;
-		enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-		enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-		enc4 = chr3 & 63;
-		if (isNaN(chr2)) enc3 = enc4 = 64;
-		else if (isNaN(chr3)) enc4 = 64;
-		output = output + key.charAt(enc1) + key.charAt(enc2) + key.charAt(enc3) + key.charAt(enc4);
-	}
-	return output;
-}
 
 $form.addEventListener("submit", e =>{
   e.preventDefault();
@@ -48,7 +24,6 @@ $form.addEventListener("submit", e =>{
   context.fillText($inputText.value, 640, 234, 850);
   const background = context.getImageData(
     0, 0, $renderer.width, $renderer.height);
-  location.hash = "progress";
 
   Promise.all(Array.from(new Array(42))
     .map((v, i) =>
@@ -73,7 +48,6 @@ $form.addEventListener("submit", e =>{
 });
 
 worker.onmessage = function (e) {
-  const url = 'data:image/gif;base64,' + encode64(e.data);
+  const url = 'data:image/gif;base64,' + e.data;
   $deliverable.src = url;
-  $download.href = url;
 };
